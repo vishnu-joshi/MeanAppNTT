6a5194a6b8056b6ed8f952ce0e95bdec72c6b613
vishnu-joshi <49960530+vishnu-joshi@users.noreply.github.com>
2019-07-23 14:12:05 -0400

    Added some input validation code so that users cannot submit invalid cities to the text box

---
+++ src/app/home/cityEntry.ts
@@ -0,0 +1,5 @@
+export class cityEntry {
+    constructor(
+        public citynameentry: string
+    ) {}
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { UserEntry } from './../user-entry';
 import { Location } from './../Location'
 import { UserentryService } from './../userentry.service'
+import { cityCrimeObject } from './CityCrimeObject'
+import { AbstractControl, Validators } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core'
+import { cityEntry } from './cityEntry';
+
 
 @Component({
   selector: 'home',
@@ -10,29 +14,50 @@ import { AgmCoreModule } from '@agm/core'
   styleUrls: ['./home.component.css'],
   providers: [UserentryService]
 })
+
+
+
 export class HomeComponent implements OnInit {
 
   usinputs: Array<UserEntry>;
   crimelocations: Array<Location>;
   latitude = 33.9806;
   longitude = -117.3755;
+  mymap = new Map<string, cityCrimeObject>();
+  cityattempt = new cityEntry("");
+  validcity = true;
+  
+  
+
   constructor(private _userEntryService: UserentryService) { }
  
   
   ngOnInit() {
-    
+      
       this._userEntryService.getInputs()
         .subscribe(resInputData => this.crimelocations = resInputData);
       console.log("this occurred");
-  }
+      this.mymap.set("Riverside, CA", new cityCrimeObject(33.9806, -117.3755, this.crimelocations));
+  };
 
-  logMessage(input: UserEntry) {
+  logMessage(input: string) {
+    /*
     console.log("Hello");
     this._userEntryService.addInput(input)
       .subscribe(resNewInput => {
         this.usinputs.push(input);
-        
+    
     });
+    */
+    if (this.mymap.has(input)) {
+      this.validcity = true
+    } else {
+      this.validcity = false
+    }
+  }
+
+  validCityValidator(control: AbstractControl): {[key: string]: any} | null {
+    return (this.mymap.has(control.value)) ? { 'forbiddenCity' : { value: control.value }} : null;
   }
   
 }
