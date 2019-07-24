848a2ac92a53d754d582b5a4dcd3a03b57e47349
vishnu-joshi <49960530+vishnu-joshi@users.noreply.github.com>
2019-07-24 16:44:31 -0400

    Changed location schema to accomodate new database format

--- server/models/loc.js
+++ server/models/loc.js
@@ -3,23 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const location = new Schema({
-    incident_id: Number,
-    case_number: String,
-    incident_datetime: String,
-    incident_type_primary: String,
-    incident_description: String,
-    clearance_type: String,
-    address_1: String,
     city: String,
-    state: String,
-    latitude: String,
-    longitude: String,
-    created_at: String,
-    updated_at: String,
-    location: Object,
-    hour_of_day: Number,
-    day_of_week: Number,
-    parent_incident_type: String
+    date: Number,
+    latitude: Number,
+    longitude: Number,
+    typeOfCrime: String
 });
 
-module.exports = mongoose.model('location', location, 'riversidejson');
+module.exports = mongoose.model('location', location, 'CityCrimes');
